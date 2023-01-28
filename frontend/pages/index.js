d785c92b39801dccbdd27bfc99ab1ed68756d9ec
taikyunn <taichi_141@me.com>
2023-01-28 13:35:02 +0900

    cssを反映

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -20,6 +20,9 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
+          <p className={styles.h1}>
+            h1スタイルテスト
+          </p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
