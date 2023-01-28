3ad91d1677c3f139b02232d3423981b67cdb4568
taikyunn <taichi_141@me.com>
2023-01-28 14:06:10 +0900

    外部cssファイルの使用

--- frontend/pages/other.js
+++ frontend/pages/other.js
@@ -1,22 +1,11 @@
-import Link from "next/link";
-
-const h1 = {
-  fontSize: '60pt',
-  fontWeight: 'bold',
-  color: 'red',
-}
-
-const p ={
-  margin: '0px',
-  color: '#666',
-  fontSize: '16pt',
-}
+import Link from "next/link"
+import other from '@/styles/Other.module.css'
 
 export default function Other() {
   return(
     <>
-      <h1 style={h1}>Next.js</h1>
-      <p style={p}>This is other page.</p>
+      <h1 className={other.h1}>Next.js</h1>
+      <p className={other.p}>This is other page.</p>
       <hr />
       <div>
         <Link href="/">
